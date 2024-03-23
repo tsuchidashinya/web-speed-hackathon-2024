f058fd9193daf4da43c29cbb71349c8bde1fc7a9
s-tsuchida-safie <s-tsuchida@safie.jp>
2024-03-23 22:30:57 +0900

    fix: 検索時の挙動を最適化

--- workspaces/app/src/features/book/components/BookListItem.tsx
+++ workspaces/app/src/features/book/components/BookListItem.tsx
@@ -9,7 +9,6 @@ import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../hooks/useBook';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -28,12 +27,24 @@ const _ImgWrapper = styled.div`
 `;
 
 type Props = {
-  bookId: string;
+  book: {
+    author: {
+      image: {
+        id: string;
+      };
+      name: string;
+    };
+    description: string;
+    id: string;
+    image: {
+      alt: string;
+      id: string;
+    };
+    name: string;
+  };
 };
 
-export const BookListItem: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
-
+export const BookListItem: React.FC<Props> = ({ book }) => {
   const imageUrl = useImage({ height: 64, imageId: book.image.id, width: 64 });
 
   return (
